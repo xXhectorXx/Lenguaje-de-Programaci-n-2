3199a4588608d46f6bb1a4bacf48e74f3efb4f9c
Héctor <hahector1505@outlook.es>
2019-05-08 07:08:10 -0500

    Update marge_sort.cpp

--- marge_sort/marge_sort.cpp
+++ marge_sort/marge_sort.cpp
@@ -11,33 +11,39 @@ int d=m+1;
 int c=0;
 while (b<=m && d<=j)
 {
-if(A[b] >= A[d])
+	if(A[b] >= A[d])
+	{
+		a[c] = A[d];
+		d++;c++;
+	}
+	else
+	{
+		a[c]=A[b];
+		b++;c++;
+	}
+}
+while (b<=m) 
 {
-a[c] = A[d];
-d++;c++;
+	a[c++]=A[b++];
 }
-else
+while (d<=j)
 {
-a[c]=A[b];
-b++;c++;
+	a[c++]=A[d++];
 }
-}
-while (b<=m) a[c++]=A[b++];
-while (d<=j) a[c++]=A[d++];
 for (int in=0;in<j-i+1;in++)
 {
-A[in+i]=a[in];
+	A[in+i]=a[in];
 }
 //memcpy(A+i,a,(j-i+1)*sizeof(int));
 }
 
 void merge_sort(int *A,int i, int j)
 {
-if (i==j) return;
-int m=(i+j)/2;
-merge_sort(A,i,m);
-merge_sort(A,m+1,j);
-merge (A,i,m,j);
+	if (i==j) return;
+	int m=(i+j)/2;
+	merge_sort(A,i,m);
+	merge_sort(A,m+1,j);
+	merge (A,i,m,j);
 }
 
 
@@ -49,8 +55,9 @@ int array[]={4,2,0,7,9,5,10};
   int *A = array;
   merge_sort(array,0, 6);
 
-for(int i=0;i<n;i++){
+for(int i=0;i<n;i++)
+{
     cout<<array[i]<<" ";
-  }
+}
 return 0;
 }
